7bf23d11f112fc0bc4ad05b23bbf4136f37b9295
Daniel Scabar <danielscabar@hotmail.com>
2022-11-23 21:13:00 -0300

    Requisição PUT microsserviços lembretes

--- backend/lembretes/index.js
+++ backend/lembretes/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
+const app = express();
 const lembretes = {};
+let contador = 0;
+
+app.use(bodyParser.json());
 
 app.get("/lembretes", (req, res) => {
   res.send(lembretes);
 });
 
-app.put("/lembretes", (req, res) => {});
+app.put("/lembretes", (req, res) => {
+  contador++;
+  const { texto } = req.body;
+  lembretes[contador] = {
+    contador,
+    texto,
+  };
+  res.status(201).send(lembretes[contador]);
+});
 
 app.listen(4000, () => {
   console.log("Lembretes. Porta 4000");
