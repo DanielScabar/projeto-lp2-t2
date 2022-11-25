d0aa48321dd41dfc58d729bb0ba6ad66b6b7de71
Daniel Scabar <danielscabar@hotmail.com>
2022-11-25 12:25:39 -0300

    Requisição PUT microsserviço observacoes

--- backend/observacoes/index.js
+++ backend/observacoes/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+const observacoesPorLembreteId = {};
 
 app.use(bodyParser.json());
 
 app.get("/lembretes/:id/observacoes", (req, res) => {});
 
-app.put("/lembretes/:id/observacoes", (req, res) => {});
+app.put("/lembretes/:id/observacoes", (req, res) => {
+  const idObs = uuidv4(); //Gera ID para a observação
+  const { texto } = req.body; //Extrai conteúdo do cabeçalho da requisição
+  const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || []; //Verifica se já existe lista de observações em um lembrete e salva.
+  observacoesDoLembrete.push({ id: idObs, texto }); //Adiciona observação na lista
+  observacoesPorLembreteId[req.params.id] = observacoesDoLembrete; //Salva lista de observações do lembrete
+  res.status(201).send(observacoesDoLembrete); //Devolve lista de observações
+});
 
 app.listen(5000, () => {
   console.log("Observacoes. Porta 5000");
