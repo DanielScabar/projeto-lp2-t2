3d2cb36e2c269dc44037ceca7ae433367b681bb4
Daniel Scabar <danielscabar@hotmail.com>
2022-11-30 20:35:24 -0300

    Barramento de eventos armazena eventos

--- backend/barramento-de-eventos/index.js
+++ backend/barramento-de-eventos/index.js
@@ -4,11 +4,13 @@ const bodyParser = require("body-parser");
 const axios = require("axios"); //Para enviar eventos para os demais microsserviços
 
 const app = express();
+const eventos = [];
 
 app.use(bodyParser.json());
 
 app.post("/eventos", (req, res) => {
   const evento = req.body;
+  eventos.push(evento);
   //envia o evento para o microsserviço de lembretes
   axios.post("http://localhost:4000/eventos", evento);
   //envia o evento para o microsserviço de observações
@@ -18,6 +20,10 @@ app.post("/eventos", (req, res) => {
   //envia o evento para o microsservico de classificacao
   axios.post("http://localhost:7000/eventos", evento);
   res.status(200).send({ msg: "ok" });
+});
+
+app.get("/eventos", (req, res) => {
+  res.send(eventos);
 });
 
 app.listen(10000, () => {
